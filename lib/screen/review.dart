09fd96af6b7e2919deca2d93a756e19e6f340c94
RachataS <64030161@kmitl.ac.th>
2023-09-10 23:39:17 +0700

    add  2 mode button

--- lib/screen/review.dart
+++ lib/screen/review.dart
@@ -12,21 +12,40 @@ class _reviewPageState extends State<reviewPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
-              width: 200,
-              height: 200,
-              child: ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue[400],
-                      fixedSize: const Size(300, 50),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50))),
-                  onPressed: () {},
-                  icon: Icon(Icons.workspaces_filled),
-                  label: Text(" Multiple Choice")),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: SizedBox(
+                width: 180,
+                height: 180,
+                child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue[400],
+                        fixedSize: const Size(300, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50))),
+                    onPressed: () {},
+                    icon: Icon(Icons.workspaces_filled),
+                    label: Text(" Multiple Choice")),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: SizedBox(
+                width: 180,
+                height: 180,
+                child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue[400],
+                        fixedSize: const Size(300, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50))),
+                    onPressed: () {},
+                    icon: Icon(Icons.abc),
+                    label: Text(" Match Card")),
+              ),
             ),
           ],
         ),
