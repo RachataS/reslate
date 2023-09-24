d75f967c67ef80e10b65ec76fcfedb0962f3e494
RachataS <64030161@kmitl.ac.th>
2023-09-24 19:32:03 +0700

    add simple words, not show true words and count ans

--- lib/models/Questions.dart
+++ lib/models/Questions.dart
@@ -9,4 +9,30 @@ class Question {
 
 Question question = Question();
 
-List sample_data = question.questionList ?? [];
+List sample_data = question.questionList ??
+    [
+      {
+        "id": 1,
+        "question": "apple",
+        "options": ['แอปเปิ้ล', 'พระเจ้า', 'มะม่วง', 'แมว'],
+        "answer_index": 0,
+      },
+      {
+        "id": 2,
+        "question": "god",
+        "options": ['แมว', 'พระเจ้า', 'คน', 'เชื่อมต่อ'],
+        "answer_index": 1
+      },
+      {
+        "id": 3,
+        "question": "connect",
+        "options": ['ผู้ชาย', 'หมา', 'เครื่องใช้ไฟฟ้า', 'เชื่อมต่อ'],
+        "answer_index": 4,
+      },
+      {
+        "id": 4,
+        "question": "jar",
+        "options": ['ไห', 'แก้ว', 'แว่น', 'แป้นพิมพ์'],
+        "answer_index": 0,
+      },
+    ];
