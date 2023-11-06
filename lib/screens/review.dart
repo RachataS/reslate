4ee05719904bedd48f06c1dbcc78dedace389554
RachataS <64030161@kmitl.ac.th>
2023-11-06 15:19:37 +0700

    back to old random

--- lib/screens/review.dart
+++ lib/screens/review.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:reslate/screens/review/matchCard.dart';
+import 'dart:math';
 
 import 'package:reslate/screens/review/multipleChoice.dart';
 
@@ -37,7 +40,7 @@ class _reviewPageState extends State<reviewPage> {
                 child: Text(
                   "Select Mode",
                   style: TextStyle(
-                    color: Colors.white,
+                    color: Colors.white!,
                     fontSize: 20,
                     fontWeight: FontWeight.w600,
                   ),
@@ -160,12 +163,12 @@ class _reviewPageState extends State<reviewPage> {
                             onPressed: () async {
                               SystemSound.play(SystemSoundType.click);
 
+                              await getSavedWords(numberOfQuestion);
                               Navigator.push(context,
                                   MaterialPageRoute(builder: (context) {
                                 return multipleChoice(
                                   docID: widget.docID,
                                   savedWordsData: true,
-                                  numberOfQuestion: numberOfQuestion,
                                 );
                               }));
                             },
@@ -186,12 +189,12 @@ class _reviewPageState extends State<reviewPage> {
                             onPressed: () async {
                               SystemSound.play(SystemSoundType.click);
 
+                              await getSavedWords(numberOfQuestion);
                               Navigator.push(context,
                                   MaterialPageRoute(builder: (context) {
                                 return multipleChoice(
                                   docID: widget.docID,
                                   savedWordsData: false,
-                                  numberOfQuestion: numberOfQuestion,
                                 );
                               }));
                             },
@@ -279,5 +282,115 @@ class _reviewPageState extends State<reviewPage> {
         ),
       ),
     );
+  }
+
+  Future<void> getSavedWords(int numberOfQuestion) async {
+    List<Map<String, dynamic>> savedWords = [];
+    print(numberOfQuestion);
+
+    DocumentReference<Map<String, dynamic>> userDocumentRef =
+        FirebaseFirestore.instance.collection("Profile").doc(widget.docID);
+    QuerySnapshot<Map<String, dynamic>> savedWordsQuerySnapshot =
+        await userDocumentRef.collection("savedWords").get();
+
+    savedWordsQuerySnapshot.docs
+        .forEach((QueryDocumentSnapshot<Map<String, dynamic>> doc) {
+      Map<String, dynamic> data = doc.data();
+      savedWords.add(data);
+    });
+
+    if (savedWords.isNotEmpty) {
+      var wordLength;
+      await userDocumentRef
+          .get()
+          .then((DocumentSnapshot<Map<String, dynamic>> document) {
+        if (document.exists) {
+          Map<String, dynamic> data = document.data()!;
+          wordLength = data["wordLength"];
+        }
+      }).catchError((error) {
+        print("Error getting document: $error");
+      });
+
+      Random random = Random();
+
+      for (int a = 0; a < wordLength; a++) {
+        Map<String, dynamic> randomWord = savedWords[a];
+        String thaiKey = randomWord['thai'];
+        String engKey = randomWord['question'];
+        String thaiKey1, thaiKey2, thaiKey3;
+        List<dynamic> reviewList = randomWord['options'];
+
+        try {
+          if (reviewList.length < 5) {
+            // Initialize a list to store the random indices
+            List<int> randomIndices = [];
+
+            while (randomIndices.length < 3) {
+              int randomIndex;
+              do {
+                randomIndex = random.nextInt(savedWords.length);
+              } while (randomIndex == a ||
+                  randomIndices.contains(randomIndex)); // Avoid duplicates
+              randomIndices.add(randomIndex);
+            }
+
+            // Get the Thai keys for the randomly selected incorrect answers
+            List randomThaiKeys = randomIndices
+                .map((index) => savedWords[index]['thai'])
+                .toList();
+
+            // Ensure that the correct answer is not in the list of incorrect answers
+            randomThaiKeys.remove(thaiKey);
+
+            thaiKey1 = randomThaiKeys[0];
+            thaiKey2 = randomThaiKeys[1];
+            thaiKey3 = randomThaiKeys[2];
+
+            await saveChoice(engKey, thaiKey, thaiKey1, thaiKey2, thaiKey3);
+          }
+        } catch (e) {
+          print('random choice error ${e}');
+          Fluttertoast.showToast(msg: '${e}', gravity: ToastGravity.TOP);
+        }
+      }
+    } else {
+      print('No savedWords available.');
+    }
+  }
+
+  Future<void> saveChoice(
+    question,
+    correctAnswer,
+    answer1,
+    answer2,
+    answer3,
+  ) async {
+    CollectionReference<Map<String, dynamic>> userCollection =
+        FirebaseFirestore.instance.collection("Profile");
+
+    try {
+      DocumentReference<Map<String, dynamic>> newDocumentRef = userCollection
+          .doc(widget.docID)
+          .collection("savedWords")
+          .doc(question);
+
+      // Create an array of answers with the correct answer included
+      List<String> answerArray = [correctAnswer, answer1, answer2, answer3];
+
+      // Shuffle the answerArray to randomize the order
+      answerArray.shuffle();
+
+      // Find the index of the correct answer within the shuffled array
+      int correctAnswerIndex = answerArray.indexOf(correctAnswer);
+
+      Map<String, dynamic> dataToStore = {
+        "options": answerArray,
+        "answer_index": correctAnswerIndex,
+      };
+      await newDocumentRef.set(dataToStore, SetOptions(merge: true));
+    } catch (e) {
+      print(e);
+    }
   }
 }
--- lib/screens/review/multipleChoice.dart
+++ lib/screens/review/multipleChoice.dart
@@ -1,8 +1,5 @@
-import 'dart:math';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:reslate/controllers/question_controller.dart';
 import 'package:reslate/screens/review/components/body.dart';
@@ -10,11 +7,11 @@ import 'package:reslate/screens/review/components/body.dart';
 class multipleChoice extends StatefulWidget {
   final String? docID;
   final bool? savedWordsData;
-  final int? numberOfQuestion;
-  multipleChoice(
-      {required this.docID,
-      required this.savedWordsData,
-      required this.numberOfQuestion});
+
+  multipleChoice({
+    required this.docID,
+    required this.savedWordsData,
+  });
 
   @override
   State<multipleChoice> createState() => _multipleChoiceState();
@@ -27,9 +24,7 @@ class _multipleChoiceState extends State<multipleChoice> {
   @override
   void initState() {
     super.initState();
-    getQuestion();
     print("from multi = ${widget.savedWordsData}");
-    getSavedWords(widget.numberOfQuestion);
   }
 
   Widget build(BuildContext context) {
@@ -107,116 +102,6 @@ class _multipleChoiceState extends State<multipleChoice> {
     } catch (e) {
       print("Error fetching data: $e");
       return []; // Return an empty list on error
-    }
-  }
-
-  Future<void> getSavedWords(numberOfQuestion) async {
-    List<Map<String, dynamic>> savedWords = [];
-    print(numberOfQuestion);
-
-    DocumentReference<Map<String, dynamic>> userDocumentRef =
-        FirebaseFirestore.instance.collection("Profile").doc(widget.docID);
-    QuerySnapshot<Map<String, dynamic>> savedWordsQuerySnapshot =
-        await userDocumentRef.collection("savedWords").get();
-
-    savedWordsQuerySnapshot.docs
-        .forEach((QueryDocumentSnapshot<Map<String, dynamic>> doc) {
-      Map<String, dynamic> data = doc.data();
-      savedWords.add(data);
-    });
-
-    if (savedWords.isNotEmpty) {
-      var wordLength;
-      await userDocumentRef
-          .get()
-          .then((DocumentSnapshot<Map<String, dynamic>> document) {
-        if (document.exists) {
-          Map<String, dynamic> data = document.data()!;
-          wordLength = data["wordLength"];
-        }
-      }).catchError((error) {
-        print("Error getting document: $error");
-      });
-
-      Random random = Random();
-
-      for (int a = 0; a < wordLength; a++) {
-        Map<String, dynamic> randomWord = savedWords[a];
-        String thaiKey = randomWord['thai'];
-        String engKey = randomWord['question'];
-        String thaiKey1, thaiKey2, thaiKey3;
-        List<dynamic> reviewList = randomWord['options'];
-
-        try {
-          if (reviewList.length < 5) {
-            // Initialize a list to store the random indices
-            List<int> randomIndices = [];
-
-            while (randomIndices.length < 3) {
-              int randomIndex;
-              do {
-                randomIndex = random.nextInt(savedWords.length);
-              } while (randomIndex == a ||
-                  randomIndices.contains(randomIndex)); // Avoid duplicates
-              randomIndices.add(randomIndex);
-            }
-
-            // Get the Thai keys for the randomly selected incorrect answers
-            List randomThaiKeys = randomIndices
-                .map((index) => savedWords[index]['thai'])
-                .toList();
-
-            // Ensure that the correct answer is not in the list of incorrect answers
-            randomThaiKeys.remove(thaiKey);
-
-            thaiKey1 = randomThaiKeys[0];
-            thaiKey2 = randomThaiKeys[1];
-            thaiKey3 = randomThaiKeys[2];
-
-            await saveChoice(engKey, thaiKey, thaiKey1, thaiKey2, thaiKey3);
-          }
-        } catch (e) {
-          print('random choice error ${e}');
-          Fluttertoast.showToast(msg: '${e}', gravity: ToastGravity.TOP);
-        }
-      }
-    } else {
-      print('No savedWords available.');
-    }
-  }
-
-  Future<void> saveChoice(
-    question,
-    correctAnswer,
-    answer1,
-    answer2,
-    answer3,
-  ) async {
-    CollectionReference<Map<String, dynamic>> userCollection =
-        FirebaseFirestore.instance.collection("Profile");
-
-    try {
-      DocumentReference<Map<String, dynamic>> newDocumentRef = userCollection
-          .doc(widget.docID)
-          .collection("savedWords")
-          .doc(question);
-
-      // Create an array of answers with the correct answer included
-      List<String> answerArray = [correctAnswer, answer1, answer2, answer3];
-
-      // Shuffle the answerArray to randomize the order
-      answerArray.shuffle();
-
-      // Find the index of the correct answer within the shuffled array
-      int correctAnswerIndex = answerArray.indexOf(correctAnswer);
-
-      Map<String, dynamic> dataToStore = {
-        "options": answerArray,
-        "answer_index": correctAnswerIndex,
-      };
-      await newDocumentRef.set(dataToStore, SetOptions(merge: true));
-    } catch (e) {
-      print(e);
     }
   }
 }
