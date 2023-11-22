626ee7983ac79df978fd811c49e48198e08d6209
RachataS <64030161@kmitl.ac.th>
2023-11-22 22:14:51 +0700

    add delete word in words Collection page

--- lib/screens/WordsCollection.dart
+++ lib/screens/WordsCollection.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:reslate/controllers/getDocument.dart';
 
 class WordsCollection extends StatefulWidget {
@@ -41,92 +43,164 @@ class _WordsCollectionState extends State<WordsCollection> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              Colors.blue[600]!,
-              Colors.blue[300]!,
-              Colors.blue[100]!,
-            ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                Colors.blue[600]!,
+                Colors.blue[300]!,
+                Colors.blue[100]!,
+              ],
+            ),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 50, right: 10, left: 10),
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 10), // Adjust this margin
-                child: TextField(
-                  onChanged: filterWords,
-                  decoration: InputDecoration(
-                    labelText: 'Search by English word',
+          child: Padding(
+            padding: const EdgeInsets.only(top: 50, right: 10, left: 10),
+            child: Column(
+              children: [
+                Container(
+                  child: TextField(
+                    onChanged: filterWords,
+                    decoration: InputDecoration(
+                      labelText: 'Search by English word',
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: filteredWords.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      margin: const EdgeInsets.all(5),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    '${index + 1} : ${filteredWords[index]['question']}',
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    '${filteredWords[index]['thai']}',
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w500),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    'Correct: ${filteredWords[index]['answerCorrect']}',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    'Wrong: ${filteredWords[index]['answerWrong']}',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: filteredWords.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                      ),
-                    );
-                  },
+                        margin: const EdgeInsets.all(5),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      '${index + 1} : ${filteredWords[index]['question']}',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      '${filteredWords[index]['thai']}',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      'Correct: ${filteredWords[index]['answerCorrect']}',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          'Wrong: ${filteredWords[index]['answerWrong']}',
+                                          style: TextStyle(fontSize: 16),
+                                        ),
+                                        Spacer(),
+                                        IconButton(
+                                          onPressed: () => deleteWord(index),
+                                          icon: Icon(Icons.delete),
+                                          color: Colors.red,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> deleteWord(index) async {
+    final subcollectionReference = FirebaseFirestore.instance
+        .collection('Profile')
+        .doc(docID)
+        .collection("savedWords");
+
+    await Get.dialog(
+      AlertDialog(
+        backgroundColor: Colors.red[100]!,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        title: Text(
+            'Do you want to delete the word ${filteredWords[index]['question']}?'),
+        content: Text(
+            'If you delete the word, ${filteredWords[index]['question']} will disappear.'),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () async {
+                  Get.back(); // Close the dialog
+                },
+                child: Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final savedWordsQuerySnapshot = await FirebaseFirestore
+                      .instance
+                      .collection("Profile")
+                      .doc(docID)
+                      .collection("savedWords")
+                      .get();
+
+                  await FirebaseFirestore.instance
+                      .collection('Profile')
+                      .doc(docID)
+                      .update({'wordLength': savedWordsQuerySnapshot.size - 1});
+
+                  await subcollectionReference
+                      .doc('${filteredWords[index]['question']}')
+                      .delete();
+                  Get.back(); // Close the dialog
+                },
+                child: Text(
+                  'Delete',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+    setState(() {});
   }
 }
