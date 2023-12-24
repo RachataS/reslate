d38323337f53367aa79954c165e2818e149c5f8d
RachataS <64030161@kmitl.ac.th>
2023-12-24 18:32:22 +0700

    config to web test

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,13 @@ import 'package:reslate/screens/bottomBar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      // options: FirebaseOptions(
+      //     apiKey: 'AIzaSyCCOBglvd14ORH2RrCjO1nosCPkE7yIYTE',
+      //     appId: '1:400328617499:web:7a1fcb9943002caaaf12be',
+      //     messagingSenderId: '400328617499',
+      //     projectId: 'reslate-7d93b')
+      );
   runApp(MyApp());
 }
 
